3cee218314c6131e93d4a6b605746051780e2ef7
Lylia <taleb.l06@outlook.com>
2019-02-09 17:49:02 +0100

    create Contact Component

---
+++ resources/assets/js/components/Contact.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        hello Contact Form
+    </div>
+</template>
+<script>
+export default 
+{
+    data(){
+        return{}
+    },
+    mounted() {
+        console.log("contact hello");
+        
+    },
+    
+}
+</script>
